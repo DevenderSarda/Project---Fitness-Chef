2020414bd504be082f05dc6742ab24a9c7068ae4
Nagumkc <nandigam.nag@gmail.com>
2017-02-16 00:58:31 -0600

    commit

--- Source/FitnessChef/app/src/main/java/com/innovators/fitnesschef/LoginActivity.java
+++ Source/FitnessChef/app/src/main/java/com/innovators/fitnesschef/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.innovators.fitnesschef;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ActionMenuView;
+import android.view.MenuItem;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,8 +12,26 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
      getSupportActionBar().setLogo(R.drawable.ic_arrow_back_white_24dp);
-        getSupportActionBar().setDisplayUseLogoEnabled(true);
+      getSupportActionBar().setDisplayUseLogoEnabled(true);
         setContentView(R.layout.activity_login);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                item.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                    public boolean onMenuItemClick(MenuItem item) {
+                        Intent redirect = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(redirect);
+                        return true;
+                    }
+
+                });
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
