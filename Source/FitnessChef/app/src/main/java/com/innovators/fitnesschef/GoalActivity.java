749ff49f10e107d4bc5b31970e7724c9d214741c
SyedMoin16 <slrp3@mail.umkc.edu>
2017-02-17 17:16:13 -0600

    Changes made in Signup page

--- Source/FitnessChef/app/src/main/java/com/innovators/fitnesschef/GoalActivity.java
+++ Source/FitnessChef/app/src/main/java/com/innovators/fitnesschef/GoalActivity.java
@@ -3,11 +3,11 @@ package com.innovators.fitnesschef;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Register extends AppCompatActivity {
+public class GoalActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_goal);
     }
 }
--- Source/FitnessChef/app/src/main/java/com/innovators/fitnesschef/SignUpActivity.java
+++ Source/FitnessChef/app/src/main/java/com/innovators/fitnesschef/SignUpActivity.java
@@ -3,7 +3,7 @@ package com.innovators.fitnesschef;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SignUp extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
